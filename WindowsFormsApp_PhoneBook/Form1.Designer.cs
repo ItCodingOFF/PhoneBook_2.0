f4fcb7116f66cd3742fa3a3052982b766098cad3
ItCodingOFF <onlinegta484@gmail.com>
2021-08-19 20:22:50 +0300

    7 commit

--- WindowsFormsApp_PhoneBook/Form1.Designer.cs
+++ WindowsFormsApp_PhoneBook/Form1.Designer.cs
@@ -57,11 +57,13 @@ namespace WindowsFormsApp_PhoneBook
 			this.lfio = new System.Windows.Forms.Label();
 			this.pictureBox1 = new System.Windows.Forms.PictureBox();
 			this.panel1 = new System.Windows.Forms.Panel();
+			this.button1 = new System.Windows.Forms.Button();
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.phoneBookBindingSource)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.dataSet11)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.panel1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// buttonNEW
@@ -69,9 +71,9 @@ namespace WindowsFormsApp_PhoneBook
 			this.buttonNEW.FlatAppearance.BorderSize = 0;
 			this.buttonNEW.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.buttonNEW.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.buttonNEW.Location = new System.Drawing.Point(3, 3);
+			this.buttonNEW.Location = new System.Drawing.Point(6, 3);
 			this.buttonNEW.Name = "buttonNEW";
-			this.buttonNEW.Size = new System.Drawing.Size(75, 23);
+			this.buttonNEW.Size = new System.Drawing.Size(50, 23);
 			this.buttonNEW.TabIndex = 0;
 			this.buttonNEW.Text = "New";
 			this.buttonNEW.UseVisualStyleBackColor = true;
@@ -82,9 +84,9 @@ namespace WindowsFormsApp_PhoneBook
 			this.buttonEDIT.FlatAppearance.BorderSize = 0;
 			this.buttonEDIT.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.buttonEDIT.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.buttonEDIT.Location = new System.Drawing.Point(109, 3);
+			this.buttonEDIT.Location = new System.Drawing.Point(86, 3);
 			this.buttonEDIT.Name = "buttonEDIT";
-			this.buttonEDIT.Size = new System.Drawing.Size(75, 23);
+			this.buttonEDIT.Size = new System.Drawing.Size(50, 23);
 			this.buttonEDIT.TabIndex = 1;
 			this.buttonEDIT.Text = "Edit";
 			this.buttonEDIT.UseVisualStyleBackColor = true;
@@ -95,9 +97,9 @@ namespace WindowsFormsApp_PhoneBook
 			this.buttonDELETE.FlatAppearance.BorderSize = 0;
 			this.buttonDELETE.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.buttonDELETE.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.buttonDELETE.Location = new System.Drawing.Point(207, 3);
+			this.buttonDELETE.Location = new System.Drawing.Point(216, 3);
 			this.buttonDELETE.Name = "buttonDELETE";
-			this.buttonDELETE.Size = new System.Drawing.Size(75, 23);
+			this.buttonDELETE.Size = new System.Drawing.Size(66, 23);
 			this.buttonDELETE.TabIndex = 2;
 			this.buttonDELETE.Text = "Delete";
 			this.buttonDELETE.UseVisualStyleBackColor = true;
@@ -118,6 +120,7 @@ namespace WindowsFormsApp_PhoneBook
             this.infoDataGridViewTextBoxColumn,
             this.iDDataGridViewTextBoxColumn});
 			this.dataGridView1.DataSource = this.phoneBookBindingSource;
+			this.dataGridView1.GridColor = System.Drawing.Color.Black;
 			this.dataGridView1.Location = new System.Drawing.Point(3, 400);
 			this.dataGridView1.Name = "dataGridView1";
 			this.dataGridView1.Size = new System.Drawing.Size(279, 256);
@@ -194,6 +197,7 @@ namespace WindowsFormsApp_PhoneBook
 			// 
 			this.textBoxINFORMATION.BackColor = System.Drawing.Color.Black;
 			this.textBoxINFORMATION.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textBoxINFORMATION.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.phoneBookBindingSource, "info", true));
 			this.textBoxINFORMATION.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
 			this.textBoxINFORMATION.ForeColor = System.Drawing.Color.White;
 			this.textBoxINFORMATION.Location = new System.Drawing.Point(86, 324);
@@ -216,6 +220,7 @@ namespace WindowsFormsApp_PhoneBook
 			// 
 			this.textBoxCATEGORY.BackColor = System.Drawing.Color.Black;
 			this.textBoxCATEGORY.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textBoxCATEGORY.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.phoneBookBindingSource, "category", true));
 			this.textBoxCATEGORY.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
 			this.textBoxCATEGORY.ForeColor = System.Drawing.Color.White;
 			this.textBoxCATEGORY.Location = new System.Drawing.Point(86, 285);
@@ -238,6 +243,7 @@ namespace WindowsFormsApp_PhoneBook
 			// 
 			this.textBoxADDRESS.BackColor = System.Drawing.Color.Black;
 			this.textBoxADDRESS.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textBoxADDRESS.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.phoneBookBindingSource, "address", true));
 			this.textBoxADDRESS.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
 			this.textBoxADDRESS.ForeColor = System.Drawing.Color.White;
 			this.textBoxADDRESS.Location = new System.Drawing.Point(86, 247);
@@ -260,6 +266,7 @@ namespace WindowsFormsApp_PhoneBook
 			// 
 			this.textBoxPHONE.BackColor = System.Drawing.Color.Black;
 			this.textBoxPHONE.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textBoxPHONE.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.phoneBookBindingSource, "phone", true));
 			this.textBoxPHONE.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
 			this.textBoxPHONE.ForeColor = System.Drawing.Color.White;
 			this.textBoxPHONE.Location = new System.Drawing.Point(86, 209);
@@ -282,6 +289,7 @@ namespace WindowsFormsApp_PhoneBook
 			// 
 			this.textBoxNAME.BackColor = System.Drawing.Color.Black;
 			this.textBoxNAME.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textBoxNAME.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.phoneBookBindingSource, "fullname", true));
 			this.textBoxNAME.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
 			this.textBoxNAME.ForeColor = System.Drawing.Color.White;
 			this.textBoxNAME.Location = new System.Drawing.Point(86, 171);
@@ -302,20 +310,41 @@ namespace WindowsFormsApp_PhoneBook
 			// 
 			// pictureBox1
 			// 
+			this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
 			this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
 			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-			this.pictureBox1.Location = new System.Drawing.Point(88, 32);
+			this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+			this.pictureBox1.Location = new System.Drawing.Point(83, 18);
 			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(96, 96);
+			this.pictureBox1.Size = new System.Drawing.Size(98, 98);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
 			this.pictureBox1.TabIndex = 12;
 			this.pictureBox1.TabStop = false;
 			// 
 			// panel1
 			// 
+			this.panel1.Controls.Add(this.pictureBox1);
+			this.panel1.ForeColor = System.Drawing.Color.White;
 			this.panel1.Location = new System.Drawing.Point(3, 32);
 			this.panel1.Name = "panel1";
 			this.panel1.Size = new System.Drawing.Size(279, 362);
 			this.panel1.TabIndex = 24;
+			this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+			// 
+			// button1
+			// 
+			this.button1.FlatAppearance.BorderSize = 0;
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button1.Location = new System.Drawing.Point(142, 3);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(68, 23);
+			this.button1.TabIndex = 13;
+			this.button1.Text = "Images";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click_1);
 			// 
 			// Form1
 			// 
@@ -323,24 +352,24 @@ namespace WindowsFormsApp_PhoneBook
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.Color.Black;
 			this.ClientSize = new System.Drawing.Size(285, 657);
+			this.Controls.Add(this.textBoxNAME);
+			this.Controls.Add(this.textBoxADDRESS);
+			this.Controls.Add(this.textBoxPHONE);
+			this.Controls.Add(this.button1);
 			this.Controls.Add(this.buttonSAVE);
 			this.Controls.Add(this.textBoxINFORMATION);
 			this.Controls.Add(this.label4);
 			this.Controls.Add(this.textBoxCATEGORY);
 			this.Controls.Add(this.label3);
-			this.Controls.Add(this.textBoxADDRESS);
 			this.Controls.Add(this.label2);
-			this.Controls.Add(this.textBoxPHONE);
 			this.Controls.Add(this.labelPhone);
-			this.Controls.Add(this.textBoxNAME);
 			this.Controls.Add(this.lfio);
-			this.Controls.Add(this.pictureBox1);
 			this.Controls.Add(this.dataGridView1);
 			this.Controls.Add(this.buttonDELETE);
 			this.Controls.Add(this.buttonEDIT);
 			this.Controls.Add(this.buttonNEW);
 			this.Controls.Add(this.panel1);
-			this.ForeColor = System.Drawing.Color.White;
+			this.ForeColor = System.Drawing.Color.DimGray;
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
@@ -352,6 +381,7 @@ namespace WindowsFormsApp_PhoneBook
 			((System.ComponentModel.ISupportInitialize)(this.dataSet11)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.panel1.ResumeLayout(false);
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -385,6 +415,7 @@ namespace WindowsFormsApp_PhoneBook
 		private System.Windows.Forms.Panel panel1;
 		private System.Windows.Forms.BindingSource phoneBookBindingSource;
 		private DataSet1 dataSet11;
+		private System.Windows.Forms.Button button1;
 	}
 }
 
--- WindowsFormsApp_PhoneBook/Form1.cs
+++ WindowsFormsApp_PhoneBook/Form1.cs
@@ -103,5 +103,34 @@ namespace WindowsFormsApp_PhoneBook
 			phoneBookBindingSource.DataSource = App.PhoneBook;
 			panel1.Enabled = false;
 		}
+		Bitmap image; //Bitmap для открываемого изображения
+
+		
+		private void button1_Click_1(object sender, EventArgs e)
+		{
+			OpenFileDialog open_dialog = new OpenFileDialog(); //создание диалогового окна для выбора файла
+			open_dialog.Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG|All files (*.*)|*.*"; //формат загружаемого файла
+			if (open_dialog.ShowDialog() == DialogResult.OK) //если в окне была нажата кнопка "ОК"
+			{
+				try
+				{
+					image = new Bitmap(open_dialog.FileName);
+					//вместо pictureBox1 укажите pictureBox, в который нужно загрузить изображение 
+					this.pictureBox1.Size = image.Size;
+					pictureBox1.Image = image;
+					pictureBox1.Invalidate();
+				}
+				catch
+				{
+					DialogResult rezult = MessageBox.Show("Невозможно открыть выбранный файл",
+					"Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				}
+			}
+		}
+
+		private void panel1_Paint(object sender, PaintEventArgs e)
+		{
+
+		}
 	}
 }
